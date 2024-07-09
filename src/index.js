2a1ec3e9c90fe6df81d63ce6752266ef917e0a85
sbbtd <ldc920@foxmail.com>
2024-07-09 06:28:56 +0000

    me

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.fromheregotothewholeworld.asia": dockerHub,
+  "quay.fromheregotothewholeworld.asia": "https://quay.io",
+  "gcr.fromheregotothewholeworld.asia": "https://gcr.io",
+  "k8s-gcr.fromheregotothewholeworld.asia": "https://k8s.gcr.io",
+  "k8s.fromheregotothewholeworld.asia": "https://registry.k8s.io",
+  "ghcr.fromheregotothewholeworld.asia": "https://ghcr.io",
+  "cloudsmith.fromheregotothewholeworld.asia": "https://docker.cloudsmith.io",
+  "ecr.fromheregotothewholeworld.asia": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.fromheregotothewholeworld.asia": dockerHub,
 };
 
 function routeByHosts(host) {
